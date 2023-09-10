297550711878bf8f67b3f151eea82b0360fe42a9
MarcioTahyra <marciotahyra@gmail.com>
2023-09-10 19:54:06 -0300

    imgs fix

--- src/Sobre.jsx
+++ src/Sobre.jsx
@@ -4,7 +4,7 @@ function Sobre() {
     return (
         <>
             <div>
-                <img src="/src/assets/images/pexels-cottonbro-studio-4612159.jpg" alt="Mulher cuidando da pele" />
+                <img src="../src/assets/images/pexels-cottonbro-studio-4612159.jpg" alt="Mulher cuidando da pele" />
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime magnam nostrum voluptate ipsa dolorum animi, dolor dicta voluptatibus repudiandae! Autem.</p>
             </div>
         </>
