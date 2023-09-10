df973d36718ff4f5c1376a7c852fcc571decf1d2
MarcioTahyra <marciotahyra@gmail.com>
2023-09-10 19:15:23 -0300

    projeto1

---
+++ src/Produtos.jsx
@@ -0,0 +1,21 @@
+import { } from 'react'
+
+function Produtos() {
+    return (
+        <>
+            <h3>Produto 1</h3>
+            <img src="/src/assets/images/pexels-denys-mikhalevych-11935611.jpg" alt="Vidros de creme para o rosto" />
+
+            <h3>Produto 2</h3>
+            <img src="/src/assets/images/pexels-denys-mikhalevych-11935633.jpg" alt="Protetor solar Sun Block" />
+
+            <h3>Produto 3</h3>
+            <img src="/src/assets/images/pexels-harper-sunday-5658858.jpg" alt="Produtos de Skin Care" />
+
+            <h3>Produto 4</h3>
+            <img src="/src/assets/images/pexels-vilvah-store-17656719.jpg" alt="Produtos para o rosto e protetor solar" />
+        </>
+    );
+}
+
+export default Produtos;
---
+++ src/Sobre.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Sobre() {
+    return (
+        <>
+            <div>
+                <img src="/src/assets/images/pexels-cottonbro-studio-4612159.jpg" alt="Mulher cuidando da pele" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime magnam nostrum voluptate ipsa dolorum animi, dolor dicta voluptatibus repudiandae! Autem.</p>
+            </div>
+        </>
+    );
+}
+
+export default Sobre;
