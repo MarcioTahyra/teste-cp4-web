4d15fa114c1b6e87e704e55a760c7c88fbc044dd
MarcioTahyra <marciotahyra@gmail.com>
2023-09-10 20:02:37 -0300

    imgs fix é o último, juro

--- src/Produtos.jsx
+++ src/Produtos.jsx
@@ -4,16 +4,16 @@ function Produtos() {
     return (
         <>
             <h3>Produto 1</h3>
-            <img src="../assets/images/pexels-denys-mikhalevych-11935611.jpg" alt="Vidros de creme para o rosto" />
-
+            <img src="../src/assets/images/pexels-denys-mikhalevych-11935611.jpg" alt="Vidros de creme para o rosto" />
+            
             <h3>Produto 2</h3>
-            <img src="../assets/images/pexels-denys-mikhalevych-11935633.jpg" alt="Protetor solar Sun Block" />
+            <img src="../src/assets/images/pexels-denys-mikhalevych-11935633.jpg" alt="Protetor solar Sun Block" />
 
             <h3>Produto 3</h3>
-            <img src="../assets/images/pexels-harper-sunday-5658858.jpg" alt="Produtos de Skin Care" />
+            <img src="../src/assets/images/pexels-harper-sunday-5658858.jpg" alt="Produtos de Skin Care" />
 
             <h3>Produto 4</h3>
-            <img src="../assets/images/pexels-vilvah-store-17656719.jpg" alt="Produtos para o rosto e protetor solar" />
+            <img src="../src/assets/images/pexels-vilvah-store-17656719.jpg" alt="Produtos para o rosto e protetor solar" />
         </>
     );
 }
