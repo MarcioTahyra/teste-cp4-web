a8c8a299f7e505110aca062189758190e419af6a
MarcioTahyra <marciotahyra@gmail.com>
2023-09-11 21:22:44 -0300

    import imgs

--- src/Sobre.jsx
+++ src/Sobre.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import imagem5 from '../assets/images/pexels-vilvah-store-17656719.jpg';
 
 function Sobre() {
     return (
         <>
             <div>
-                <img src="../src/assets/images/pexels-cottonbro-studio-4612159.jpg" alt="Mulher cuidando da pele" />
+                <img src={imagem5} alt="Mulher cuidando da pele" />
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime magnam nostrum voluptate ipsa dolorum animi, dolor dicta voluptatibus repudiandae! Autem.</p>
             </div>
         </>
